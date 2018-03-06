5e397ca73f3666df9e8bb0c1cb74082510a18fbe
zgr126 <zgr126@126.com>
2018-03-06 13:26:02 +0800

    RN

---
+++ string.go
@@ -0,0 +1,30 @@
+package tool
+
+import "bytes"
+
+func JoinStringWithComma(lst []string) string {
+	var buf bytes.Buffer
+	for i, v := range lst {
+		if i == 1 {
+			buf.WriteString(v)
+		} else {
+			buf.WriteString(",")
+			buf.WriteString(v)
+		}
+	}
+	return buf.String()
+}
+
+func JoinStringWithCommaEnter(lst []string) string {
+	var buf bytes.Buffer
+	for i, v := range lst {
+		if i == 1 {
+			buf.WriteString(v)
+		} else {
+			buf.WriteString(",")
+			buf.WriteString("\n")
+			buf.WriteString(v)
+		}
+	}
+	return buf.String()
+}
