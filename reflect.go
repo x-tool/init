2d08a9082b6468c3d3c601f62c97c534e0422211
zgr126 <zgr126@126.com>
2018-10-18 14:38:58 +0800

    rn

--- reflect.go
+++ reflect.go
@@ -5,3 +5,12 @@ import "reflect"
 func GetFullName(typ reflect.Type) string {
 	return typ.PkgPath() + "." + typ.Name()
 }
+
+// get real reflect.Value from raw, across ptr
+func GetRealReflectValue(raw *reflect.Value) *reflect.Value {
+	if raw.Kind() == reflect.Ptr || raw.Kind() == reflect.Interface {
+		_raw := raw.Elem()
+		raw = &_raw
+	}
+	return raw
+}
