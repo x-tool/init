4de52f9eddcfff6fdc89b09f4a7c06e0cabb9f29
zgr126 <zgr126@126.com>
2018-12-03 17:18:22 +0800

    rn

--- reflect.go
+++ reflect.go
@@ -7,9 +7,20 @@ func GetFullName(typ reflect.Type) string {
 }
 
 // get real reflect.Value from raw, across ptr
-func GetRealReflectValue(raw reflect.Value) *reflect.Value {
+func GetRealReflectValue(raw reflect.Value) reflect.Value {
 	if raw.Kind() == reflect.Ptr || raw.Kind() == reflect.Interface {
 		raw = raw.Elem()
 	}
-	return &raw
+	return raw
+}
+
+func IsComplex(r reflect.Value) bool {
+	if r.Kind() == reflect.Ptr {
+		r = r.Elem()
+	}
+	kind := r.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		return true
+	}
+	return false
 }
